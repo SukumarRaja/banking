7eaec9b9dbea618eddecaf058590c3b723729185
Sukumar Raja <sukumar@rndsoftech.com>
2025-04-09 18:23:08 +0530

    model created for response

---
+++ lib/features/auth/data/modles/auth_response.dart
@@ -0,0 +1,16 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'auth_response.g.dart';
+
+@JsonSerializable()
+class AuthResponse {
+  final String token;
+  final String userId;
+
+  AuthResponse({required this.token, required this.userId});
+
+  factory AuthResponse.fromJson(Map<String, dynamic> json) =>
+      _$AuthResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AuthResponseToJson(this);
+}
